d3f410703c9922c14e0c8fc86039f7afcfa6260d
Claudia Méndez <claudiamendezt19@gmail.com>
2023-11-07 18:56:58 -0500

    Update 04-operadores.php

--- PHP_101/04-operadores.php
+++ PHP_101/04-operadores.php
@@ -1,6 +1,23 @@
 <?php include 'includes/header.php';
 
+$number1 = 20;
+$number2 = 10;
 
+// Add
+echo $number1 + $number1;
+
+// Substraction
+echo $number1 - $number1;
+echo "<br>";
+
+// Multiply
+echo $number1 * $number2;
+
+// Divide
+echo $number1 / $number2;
+
+// Multiply more than once
+echo 2 ** 3;
 
 
 include 'includes/footer.php';
