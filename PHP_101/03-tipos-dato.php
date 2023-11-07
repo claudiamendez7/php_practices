23be9816cd5c06e4ef8e42fb065b3743224bd260
Claudia Méndez <claudiamendezt19@gmail.com>
2023-11-07 18:56:28 -0500

    Update 03-tipos-dato.php

--- PHP_101/03-tipos-dato.php
+++ PHP_101/03-tipos-dato.php
@@ -1,6 +1,23 @@
 <?php include 'includes/header.php';
 
+// Booleans
+$logged = true;
+var_dump($logged);
 
+// Integers
+$number = 200;
+var_dump($number);
+
+// Floats
+$float = 200.5;
+var_dump($float);
+
+// Strings
+$name = "Claudia";
+var_dump($name);
+
+$array = [];
+var_dump($array);
 
 
 include 'includes/footer.php';
