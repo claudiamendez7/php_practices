ce9bceb6e4f22594e35b58d15957978c49153632
Claudia Méndez <claudiamendezt19@gmail.com>
2023-11-07 18:57:30 -0500

    Update 05-comparacion.php

--- PHP_101/05-comparacion.php
+++ PHP_101/05-comparacion.php
@@ -1,5 +1,40 @@
 <?php include 'includes/header.php';
 
+$number1 = 20;
+$number2 = 30;
+$number3 = 30;
+$number4 = "30";
+
+var_dump($number1 > $number2);
+echo "<br>";
+
+var_dump($number1 < $number2);
+echo "<br>";
+
+var_dump($number1 >= $number2);
+echo "<br>";
+
+var_dump($number1 <= $number2);
+echo "<br>";
+
+var_dump($number2 == $number3);
+echo "<br>";
+
+var_dump($number2 == $number4);
+echo "<br>";
+
+var_dump($number2 === $number4);
+echo "<br>";
+
+// If -1 left is lower than right. 0 equal, 1 if left is higher
+var_dump($number1 <=> $number2);
+echo "<br>";
+
+var_dump($number2 <=> $number3);
+echo "<br>";
+
+var_dump($number2 <=> $number1);
+echo "<br>";
 
 
 
