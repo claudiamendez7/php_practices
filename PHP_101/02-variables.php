4c90a7e62da700b61cf913c764637e39b60a0441
Claudia Méndez <claudiamendezt19@gmail.com>
2023-11-07 18:55:52 -0500

    Update 02-variables.php

--- PHP_101/02-variables.php
+++ PHP_101/02-variables.php
@@ -1,6 +1,21 @@
 <?php include 'includes/header.php';
 
+$name = "Claudia";
 
+
+$name = "Claudia 2";
+
+echo $name;
+var_dump($name);
+
+define('constant', 'Value of constant');
+echo constant;
+
+const constant2 = "Hello 2";
+echo constant2;
+
+$nameClient = "Peter";
+$name_client = "Peter";
 
 
 include 'includes/footer.php';
